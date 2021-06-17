ae03c60d800a9df1a5ea5d1056c7c93851201a22
Samuelmm1997 <smenaged@grubbrr.com>
2021-06-17 07:10:22 -0400

    changed name of project

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get("x-forwarded-proto") !== "https") {
+    return res.redirect("https://" + req.get("host") + req.url);
+  }
+  next();
+}
+
 //Install express server
 const express = require("express");
 const path = require("path");
@@ -5,10 +13,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/shira-resume"));
+app.use(express.static(__dirname + "/dist/resume-project"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/shira-resume/index.html"));
+  res.sendFile("index.html", { root: "dist/resume-project>/" });
 });
 
 // Start the app by listening on the default Heroku port
